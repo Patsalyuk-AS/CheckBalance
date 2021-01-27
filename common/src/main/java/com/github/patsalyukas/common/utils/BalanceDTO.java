97bb0e9355dd25ed2f6e78e22a48e4db7f295343
Andrey Patsalyuk <pazalyuk@gmail.com>
2021-01-27 22:17:16 +0300

    Lombok NoArgsConstructor

--- common/src/main/java/com/github/patsalyukas/common/utils/BalanceDTO.java
+++ common/src/main/java/com/github/patsalyukas/common/utils/BalanceDTO.java
@@ -2,10 +2,12 @@ package com.github.patsalyukas.common.utils;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
@@ -14,12 +16,10 @@ public class BalanceDTO {
     private BigDecimal balance;
     private Currency currency;
 
+    //без конструктора по умолчанию выкидывает ошибку
+    //com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `com.github.patsalyukas.common.utils.BalanceDTO`
+    //(no Creators, like default constructor, exist): cannot deserialize from Object value (no delegate- or property-based Creator)
+    //Решение - https://facingissuesonit.com/2019/07/17/com-fasterxml-jackson-databind-exc-invaliddefinitionexception-cannot-construct-instance-of-xyz-no-creators-like-default-construct-exist-cannot-deserialize-from-object-value-no-delega/
 
-    public BalanceDTO() {
-        //без конструктора по умолчанию выкидывает ошибку
-        //com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `com.github.patsalyukas.common.utils.BalanceDTO`
-        //(no Creators, like default constructor, exist): cannot deserialize from Object value (no delegate- or property-based Creator)
-        //Решение - https://facingissuesonit.com/2019/07/17/com-fasterxml-jackson-databind-exc-invaliddefinitionexception-cannot-construct-instance-of-xyz-no-creators-like-default-construct-exist-cannot-deserialize-from-object-value-no-delega/
-    }
 
 }
