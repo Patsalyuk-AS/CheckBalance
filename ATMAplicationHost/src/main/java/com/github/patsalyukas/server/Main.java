6af65bc0d612739481b2191c04eedcbc2c989ff9
Andrey Patsalyuk <pazalyuk@gmail.com>
2021-02-08 20:50:04 +0300

    Добавил schema.sql

--- ATMAplicationHost/src/main/java/com/github/patsalyukas/server/Main.java
+++ ATMAplicationHost/src/main/java/com/github/patsalyukas/server/Main.java
@@ -1,11 +1,8 @@
 package com.github.patsalyukas.server;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@Slf4j
 @SpringBootApplication
 public class Main {
 
--- ATMAplicationHost/src/main/resources/data.sql
+++ ATMAplicationHost/src/main/resources/data.sql
@@ -1,25 +1,3 @@
-DROP TABLE IF EXISTS Balances;
-DROP TABLE IF EXISTS Bankcards;
-
-CREATE TABLE Bankcards (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  card_number VARCHAR(18) NOT NULL,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  exp_date VARCHAR(5) NOT NULL,
-  cvi VARCHAR(3) NOT NULL,
-  pin INT NOT NULL,
-  type VARCHAR(6) NOT NULL
-);
-
-CREATE TABLE Balances (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  balance NUMERIC DEFAULT 0,
-  currency VARCHAR(3) NOT NULL,
-  card_id INT NOT NULL,
-    foreign key (card_id) references Bankcards(id)
-);
-
 INSERT INTO Bankcards (card_number, first_name, last_name, exp_date, cvi, pin, type) VALUES
   ('4256123542131234', 'IVAN', 'IVANOV', '10/21', '123', '1234', 'DEBET'),
   ('4256123542134526', 'ALEXEY', 'PETROV', '09/22', '452', '4521', 'DEBET'),
